b3d249c7fd609d9ea1cca57e562799825e334b34
kiyama <tadashi.kiyama@gmail.com>
2026-06-07 15:58:28 +0900

    コミットから削除

--- registerSystem/Config/ApiEndpoints.cs
+++
@@ -1,8 +0,0 @@
-namespace registerSystem.Config;
-
-public static class ApiEndpoints
-{
-    public const string BaseUrl = "https://k-pos-system-api-func-abcydpc4b9g5a7hh.japanwest-01.azurewebsites.net/api";
-    public const string Orders = $"{BaseUrl}/orders?code=C6GJNDi4_TD0AtuK8bvITIOu5JXrupHFCiAZeri2nvw2AzFuGvcB5Q==";
-    public const string SignalRNegotiate = $"{BaseUrl}/signalr/negotiate?code=C6GJNDi4_TD0AtuK8bvITIOu5JXrupHFCiAZeri2nvw2AzFuGvcB5Q==";
-}
